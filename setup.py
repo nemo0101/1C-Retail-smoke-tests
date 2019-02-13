12d26db0fb4af01fdbee0d2ca84815c7354e983f
Kuznetsov Grigory <45321211+nemo0101@users.noreply.github.com>
2019-02-13 22:38:11 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='test_1c',
+    version='0.30',
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    entry_points={'console_scripts':['test_1c = test_1c.main:main']},
+    include_package_data=True,
+    install_requires=[
+        'selenium==3.141.0',
+        'Pillow==5.3.0',
+        'pywin32==223'
+        ],
+    license='',
+    author="Kuznetsov Gregory",
+    author_email="a925041@yandex.ru",
+    url = '',
+)
